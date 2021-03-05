be3025b03a34fa6b6b6c5bc9d409a1b76c87fe83
Kavindu Chamodh Nethsara <72559423+ChamodhNethsara@users.noreply.github.com>
2021-03-05 19:00:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 
-<script>
-    
-    function newfunc() {
+function newfunc() {
         
-        a = new Date();
-        day ="AM"
-        hours = a.getHours();
-        if (hours > 12){
-            hours = hours -12;
-            day = 'PM'
-        }
-        else{
+   a = new Date();
+   day ="AM"
+   hours = a.getHours();
+   if (hours > 12){
+       hours = hours -12;
+       day = 'PM'
+   }
+   else{
             
-        }
-        document.getElementById("hours").innerHTML =("0"+ hours).slice(-2)+':';
-        document.getElementById("minutes").innerHTML = ("0"+ a.getMinutes()).slice(-2)+':';
-        document.getElementById("seconds").innerHTML = ("0"+ a.getSeconds()).slice(-2)+"&nbsp;"+day;
+   }
+   document.getElementById("hours").innerHTML =("0"+ hours).slice(-2)+':';
+   document.getElementById("minutes").innerHTML = ("0"+ a.getMinutes()).slice(-2)+':';
+   document.getElementById("seconds").innerHTML = ("0"+ a.getSeconds()).slice(-2)+"&nbsp;"+day;
 
+}
 
-    }
-    setInterval(newfunc,1000);
-</script
+setInterval(newfunc,1000);
+
